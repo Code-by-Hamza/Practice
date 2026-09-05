3a1f42aba57be94aa08bd30fe71390f839977bd3
Hamza <code.by.hamzaa@gmail.com>
2026-09-05 14:28:22 +0500

    initial commit

---
+++ Counter/README.md
@@ -0,0 +1,14 @@
+# Getting Started
+Install the dependencies and run the project
+```
+npm install
+npm start
+```
+
+## About Project
+
+A simple counter App
+
+# Learning Javascript
+
+Happy Coding!
---
+++ Counter/index.js
@@ -0,0 +1,24 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
+let count = 0
+let subTotal = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    subTotal += count
+    totalEl.textContent = "SubTotal: " + subTotal
+    countEl.textContent = 0
+    count = 0
+
+}
+
+function total() {
+
+}
