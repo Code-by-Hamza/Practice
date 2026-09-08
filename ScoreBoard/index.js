7e215f7988d8849c5f6fa52967dc49aa9a466524
Hamza <code.by.hamzaa@gmail.com>
2026-09-08 17:11:21 +0500

    added timer

--- ScoreBoard/index.js
+++ ScoreBoard/index.js
@@ -35,4 +35,55 @@ function reset() {
     guestResult = 0
     homeScore.textContent = homeResult
     guestScore.textContent = guestResult
+    resetTimer()
+}
+
+//timer
+let timeInterval;
+let timeRemaining = 1800;
+let isRunning = false;
+
+const display = document.getElementById("timer")
+const startBtn = document.getElementById("start-btn")
+const buttonsA = document.querySelectorAll(".buttons-a")
+
+function updateDisplay() {
+    let minutes = Math.floor(timeRemaining / 60)
+    let seconds = timeRemaining % 60
+
+    let minutesStr = minutes.toString().padStart(2, "0")
+    let secondsStr = seconds.toString().padStart(2, "0")
+
+    display.textContent = `${minutesStr} : ${secondsStr}`
+}
+
+function startTimer() {
+    if (isRunning) return;
+    isRunning = true;
+    startBtn.disabled = true;
+    buttonsA.forEach(button => button.disabled = false)
+
+
+    timeInterval = setInterval(() => {
+        if (timeRemaining === 0) {
+            clearInterval(timeInterval)
+            display.textContent = "Time's Up!"
+            display.classList.add("time-up")
+            isRunning = false
+        } else {
+            timeRemaining--
+            updateDisplay()
+        }
+
+    }, 1000);
+}
+
+function resetTimer() {
+    display.classList.remove("time-up")
+    clearInterval(timeInterval);
+    timeRemaining = 1800
+    isRunning = false
+    startBtn.disabled = false
+    buttonsA.forEach(button => button.disabled = true)
+    updateDisplay()
 }
