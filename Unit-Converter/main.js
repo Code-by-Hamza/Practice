a450010c9ab894df43c843971cbea11a589e8000
Hamza <code.by.hamzaa@gmail.com>
2026-09-17 10:34:32 +0500

    add complete logic

--- Unit-Converter/main.js
+++ Unit-Converter/main.js
@@ -1,6 +1,6 @@
 /*
 1 meter = 3.281 feet
-1 liter = 0.264 gallon
+1 liters = 0.264 gallon
 1 kilogram = 2.204 pound
 */
 const numberInput = document.getElementById("input-el");
@@ -19,16 +19,28 @@ numberInput.addEventListener('input-el', function () {
 
 btnEl.addEventListener("click", () => {
     const inputValue = numberInput.valueAsNumber
-    lengthEl.textContent = length(inputValue)
+    lengthEl.textContent = lengthF(inputValue)
+    volumeEl.textContent = volumeF(inputValue)
+    massEl.textContent = massF(inputValue)
     console.log(inputValue)
 
 })
 
 //convertion functions
-function length(input) {
+function lengthF(input) {
     let feet = input * 3.281
     let meters = input / 3.281
     return `${input} meters = ${feet.toFixed(3)} | ${input} feet = ${meters.toFixed(3)} meters`
+}
+function volumeF(input) {
+    let gallons = input * 0.264
+    let liters = input / 0.264
+    return `${input} gallons = ${liters.toFixed(3)} liters | ${input} liters = ${gallons.toFixed(3)} gallons`
+}
+function massF(input) {
+    let pounds = input * 2.204
+    let kilos = input / 2.204
+    return `${input} kilos = ${pounds.toFixed(3)} pounds | ${input} pounds = ${kilos.toFixed(3)} kilos`
 }
 
 
