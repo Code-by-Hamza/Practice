902d4972ac71c760fc655b9844bc8bf497b9d139
Hamza <code.by.hamzaa@gmail.com>
2026-09-22 16:50:30 +0500

    render posts from data

--- Oldagram/index.js
+++ Oldagram/index.js
@@ -1,57 +1,85 @@
+// Posts Data
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 475
+    },
+    {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
 
+// Html
+const feed = document.querySelector("#feed");
+posts.forEach(post => {
+    const html = `
+    <section>
+        <div class="container">
+            <img class="avatar" src="${post.avatar}">
+            <div>
+                <span class="name bold-text">${post.name}</span><br>
+                <span class="location">${post.location}</span>
+            </div>
+        </div>
+        <div class="img-container">
+            <img class="post" src="${post.post}" alt="A Selfie Post.">
+            <img class="overlay-heart" src="images/heart.png">
+        </div>
+        <div class="container-two">
+            <div>
+                <button class="like-btn heart-icon"></button>
+                <img class="icon" src="images/icon-comment.png">
+                <img class="icon" src="images/icon-dm.png">
+            </div>
+            <div class="likes-container">
+                <span class="likes bold-text">${post.likes} Likes</span>
+            </div>
+            <p><span class="bold-text">${post.username}</span> ${post.comment}</p>
+        </div>
+    </section>
+    `;
+    feed.innerHTML += html;
+})
 
+// Likes logic
 const likeBtns = document.querySelectorAll(".like-btn");
 const postContainers = document.querySelectorAll(".img-container")
 const likesContainers = document.querySelectorAll(".likes-container")
-let liked = false
 
-// like icon color
 likeBtns.forEach(likeBtn => {
     likeBtn.addEventListener("click", () => {
         const section = likeBtn.closest("section");
-        const likes = section.querySelector(".likes");
-        if (!liked) {
-            // Increment the likes
-            let currentLikes = parseInt(likes.textContent) || 0;
-            likes.textContent = (currentLikes + 1) + " Likes";
-            liked = true
-            section.querySelector(".like-btn").classList.toggle("liked")
-
-        } else {
-            // Decrement the likes
-            currentLikes = parseInt(likes.textContent) || 0;
-            likes.textContent = (currentLikes - 1) + " Likes";
-            liked = false
-            section.querySelector(".like-btn").classList.toggle("liked")
-        }
+        toggleLike(section)
     });
 });
 
-
-//posts heart effect
 postContainers.forEach(container => {
     container.addEventListener("dblclick", () => {
         const overlayHeart = container.querySelector(".overlay-heart")
         const section = container.closest("section");
-        const likes = section.querySelector(".likes");
-        if (!liked) {
-            // Increment the likes
-            let currentLikes = parseInt(likes.textContent) || 0;
-            likes.textContent = (currentLikes + 1) + " Likes";
-            liked = true
-            section.querySelector(".like-btn").classList.toggle("liked")
-
-        } else {
-            // Decrement the likes
-            currentLikes = parseInt(likes.textContent) || 0;
-            likes.textContent = (currentLikes - 1) + " Likes";
-            liked = false
-            section.querySelector(".like-btn").classList.toggle("liked")
-        }
-
+        toggleLike(section)
 
         overlayHeart.classList.remove("is-hiding", "is-visible");
-
         void overlayHeart.offsetWidth
 
         overlayHeart.classList.add("is-visible");
@@ -67,22 +95,22 @@ postContainers.forEach(container => {
     })
 })
 
-// increase likes 
-function increaseLikes() {
-    likesContainers.forEach(container => {
+function toggleLike(section) {
+    const likes = section.querySelector(".likes");
+    const likeBtn = section.querySelector(".like-btn");
 
-        const likes = container.querySelector(".likes")
-
+    if (!likeBtn.classList.contains("liked")) {
+        // Increment the likes
         let currentLikes = parseInt(likes.textContent) || 0;
-        likes.textContent = currentLikes + 1;
-        likes.textContent += " Likes"
-        console.log(likes)
-    })
+        likes.textContent = (currentLikes + 1) + " Likes";
+        liked = true
+        section.querySelector(".like-btn").classList.toggle("liked")
+
+    } else {
+        // Decrement the likes
+        currentLikes = parseInt(likes.textContent) || 0;
+        likes.textContent = (currentLikes - 1) + " Likes";
+        liked = false
+        section.querySelector(".like-btn").classList.toggle("liked")
+    }
 }
-
-
-
-//const text = "(32 Likes)";
-//const count = parseInt(text.match(/\d+/)[0], 10);
-
-//console.log(count); // Output: 32
