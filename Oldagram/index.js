a215002a98552845bf1f1b25a41f5ac841c5277d
Hamza <code.by.hamzaa@gmail.com>
2026-09-22 11:41:02 +0500

    add heart effect

--- Oldagram/index.js
+++ Oldagram/index.js
@@ -2,9 +2,33 @@
 
 const likeBtns = document.querySelectorAll(".like-btn");
 
-
 likeBtns.forEach(likeBtn => {
     likeBtn.addEventListener("click", () => {
         likeBtn.classList.toggle("liked");
     });
 });
+
+const postContainers = document.querySelectorAll(".img-container")
+
+postContainers.forEach(container => {
+    container.addEventListener("dblclick", () => {
+        const overlayHeart = container.querySelector(".overlay-heart")
+
+        if (!overlayHeart) return;
+
+        overlayHeart.classList.remove("is-hiding", "is-visible");
+
+        void overlayHeart.offsetWidth
+
+        overlayHeart.classList.add("is-visible");
+
+        setTimeout(() => {
+            overlayHeart.classList.add("is-hiding");
+
+            overlayHeart.addEventListener("animationend", function handler() {
+                overlayHeart.classList.remove("is-hiding", "is-visible");
+                overlayHeart.removeEventListener("animationend", handler)
+            }, { once: true })
+        }, 700);
+    })
+})
