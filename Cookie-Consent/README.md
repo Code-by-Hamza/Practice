0b7c794aa0bb276c1bddf590ff68ffa173315f9f
Hamza <code.by.hamzaa@gmail.com>
2026-09-24 11:14:57 +0500

    ui tweaks & add screenshot

--- Cookie-Consent/README.md
+++ Cookie-Consent/README.md
@@ -1,6 +1,7 @@
 # Cookie Consent
 
-This project has no purpose. This is just a fun little meme project.
+I present you the most annoying popup. This is just a fun little meme project.
 
+![Cookie Consent Screenshot](images/screenshot.png)
 
 Built with HTML, CSS, and JavaScript.
--- Cookie-Consent/index.js
+++ Cookie-Consent/index.js
@@ -2,6 +2,8 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 setTimeout(function () {
     modal.style.display = 'inline'
@@ -11,8 +13,16 @@ modalCloseBtn.addEventListener('click', function () {
     modal.style.display = 'none'
 })
 
+declineBtn.addEventListener('mouseenter', function () {
+    modalChoiceBtns.classList.toggle('modal-btns-reverse')
+})
+
 consentForm.addEventListener('submit', function (e) {
     e.preventDefault()
+
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
@@ -20,15 +30,20 @@ consentForm.addEventListener('submit', function (e) {
     </div>`
 
     setTimeout(function () {
-        document.getElementById('upload-text').innerText = "Making the sale..."
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
     }, 1500)
 
-    setTimeout(() => {
-        document.getElementById("modal-inner").innerHTML = `
-        <h2>Thanks you sucker! </h2>
+
+    setTimeout(function () {
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate.gif">
-        </div>`
-    }, 3000);
+        </div>
+    `
+        modalCloseBtn.disabled = false
+    }, 3000)
+
 }) 
